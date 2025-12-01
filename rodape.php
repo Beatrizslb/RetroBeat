6ebf4ff604e93c97f7bd32dce6e3b6f364dc13a3
Stephanyvitt <stephanyvitt@gmail.com>
2025-12-01 20:25:58 -0300

    Add head section to rodape.php

--- rodape.php
+++ rodape.php
@@ -1,4 +1,9 @@
-    <footer>
+<head>
+    <meta charset="UTF-8">
+    <title>Retrobeat</title>
+    <link rel="stylesheet" href="retrobeat.css">
+</head>
+<footer>
         <p>&copy; <?= date("Y") ?>Todos os direitos reservados.</p>
     </footer>
 </body>
