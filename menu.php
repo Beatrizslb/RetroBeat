01c65d42d1d57f9c7bebe4b1679999f85216c142
Stephanyvitt <stephanyvitt@gmail.com>
2025-12-01 20:24:26 -0300

    Refactor navigation menu and add toggle functionality

--- menu.php
+++ menu.php
@@ -1,9 +1,30 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Retrobeat</title>
+    <link rel="stylesheet" href="retrobeat.css">
+</head>
+
 <link rel="stylesheet" href="assets/css/global.css">
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="?pg=produtos">Produtos</a></li>
-        <li><a href="?pg=quemsomos">Quem somos</a></li>
-        <li><a href="?pg=faleconosco">Contato</a></li>
-    </ul>
-</nav>
+<header>
+    
+    <div class="logo">Retrobit</div>
+
+    <div class="toggle">☰</div>
+
+    <nav class="menu">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="?pg=produtos">Produtos</a></li>
+            <li><a href="?pg=quemsomos">Quem somos</a></li>
+            <li><a href="?pg=faleconosco">Contato</a></li>
+        </ul>
+    </nav>
+    <script>
+    const toggle = document.querySelector(".toggle");
+    const menu = document.querySelector(".menu");
+
+    toggle.addEventListener("click", () => {
+        menu.classList.toggle("ativo");
+    });
+    </script>
+</header>
