5a68296064249ed4ea6bc78cb507660ba4cc8f78
dans083 <danielcostasantos234@gmail.com>
2025-12-01 18:10:01 -0300

    Update faleconosco.php
    Updated contact information and added social media details.

--- faleconosco.php
+++ faleconosco.php
@@ -1,3 +1,19 @@
 <section class="contato">
-    
+    <h1>Fale Conosco</h1>
+
+    <p>Entre em contato com a <strong>RetroVibe Discos</strong>. Será um prazer atender você!</p>
+
+    <h3>📞 Telefone</h3>
+    <p>(83) 3456-7823</p>
+
+    <h3>📍 Endereço</h3>
+    <p>Rua Harmonia, 430 – Manaíra, João Pessoa– PB</p>
+
+    <h3>📸 Instagram</h3>
+    <p>@retrovibe.atendimento</p>
+
+    <h3>📧 E-mail</h3>
+    <p>contato@retrovibediscos.com</p>
+
+    <p>Responderemos o mais rápido possível!</p>
 </section>
